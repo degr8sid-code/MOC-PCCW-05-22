a0d30ec88a0d300c52428fde69a04345a877e649
SidrahAbdullah <hsabdullah0@gmail.com>
2022-09-06 09:26:51 +0500

    Add week13 files

---
+++ week13/formikform/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type = "text" id='name'/>
+        <input type = "text" id='email'/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
