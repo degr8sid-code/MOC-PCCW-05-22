6465acb83215f3df2c91aa990e3f1ff9597f7752
SidrahAbdullah <hsabdullah0@gmail.com>
2022-12-28 22:14:50 +0500

    configure frontend and backend

--- week23/server/index.js
+++ week23/server/index.js
@@ -1,10 +1,22 @@
+const path = require('path')
 const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require(swagger-ui-express);
 const app = express();
 const port = 4000;
+//lowdb code
+// const low = require("lowdb");
+// const fs = require("lowdb/adapters/FileSync");
+// const adapter = new fs("db.json")
+// const db = low(adapter)
+
+// parser for POST
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //swagger setup
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const swaggerOptions = {
     swaggerDefinition : {
         info: {
@@ -16,31 +28,80 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-//lowdb code
-const low = require("lowdb");
-const fs = require("lowdb/adapters/FileSync");
-const adapter = new fs("db.json")
-const db = low(adapter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 //local routes
 app.get('/', (req,res) => {
     res.send('Hello World')
+    // res.send("Some change")
     //displaying lowdb result on browser
     // const results = db.get("items").value()
     // res.send(results)
 });
 
-app.get('/lists', (req, res) => {
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *      description: get list of tasks
+ *      responses: 
+ *          200:
+ *              description: success
+ */
+
+app.get('/tasks', (req, res) => {
     res.send([
         {
-            task: "edit project"
+            id: 1,
+            task: "edit project",
+            priority: "medium"
+        }, 
+        {
+            id:2,
+            task: "prepare lunch",
+            priority: "high"
+        },
+        {
+            id:3,
+            task: "take MOC session",
+            priority: "high"
         }
+
     ])
 });
 
+/**
+ * @swagger
+ * /task:
+ *  post:
+ *      description: get one task 
+ *      parameters:
+ *      - name: id
+ *        description: id of the task
+ *        in: body
+ *        required: true
+ *        type: string          
+ *      - name: task
+ *        description: one task
+ *        in: body
+ *        required: true
+ *        type: string
+ *      responses: 
+ *          200:
+ *              description: success
+ */
 //post route
-
+app.post('/task', (req, res) => {
+    // const task = {
+    //     id: req.body.id,
+    //     todo: req.body.task
+    // }
+    const id = req.body.id;
+    const task = req.body.task;
+    // const priority = req.body.priority;
+    res.send({"id":id, "task":task});
+})
 //JSON web token
 //OAUTH2
 
